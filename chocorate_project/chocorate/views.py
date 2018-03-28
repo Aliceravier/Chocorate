f91effcd72930799d6c0889c0a7c27184e77c3e1
Mihaela Petrova <2270488p@student.gla.ac.uk>
2018-03-28 05:13:56 +0300

    message added to home after logging in

--- chocorate_project/chocorate/views.py
+++ chocorate_project/chocorate/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import logout
+from django.core.urlresolvers import reverse
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.forms import PasswordChangeForm
