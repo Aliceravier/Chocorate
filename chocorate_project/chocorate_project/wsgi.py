6d82af6b5a194017386ced3396b76acf5c1490f9
2267429j <2267429j@24dcaea7d349>
2018-03-28 12:07:50 +0000

    signup not shit

--- chocorate_project/chocorate_project/wsgi.py
+++ chocorate_project/chocorate_project/wsgi.py
@@ -1,16 +1,15 @@
-"""
-WSGI config for chocorate_project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
-"""
 
 import os
+import sys
 
+# assuming your Django settings file is at '/home/myusername/mysite/mysite/settings.py'
+path = '/home/2267429j/Chocorate/chocorate_project'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'chocorate_project.settings'
+
+## Uncomment the lines below depending on your Django version
+###### then, for Django >=1.5:
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chocorate_project.settings")
-
 application = get_wsgi_application()
